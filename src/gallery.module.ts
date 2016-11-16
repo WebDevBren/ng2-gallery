a7a29e7bf157400b355c2194733eab0ddc06a5de
Brendan Ingham <brendan@the-tool-shed.uk>
2016-11-16 14:39:46 +0000

    something weird happened with the galleryModule

---
+++ src/gallery.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { GalleryComponent } from './gallery.component';
+
+@NgModule({
+    declarations: [
+        GalleryComponent
+    ]
+})
+export class GalleryModule {
+
+    constructor() {}
+
+}
